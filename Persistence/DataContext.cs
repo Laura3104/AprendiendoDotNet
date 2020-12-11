5b07e0e6cabe840a4e17fb4a419ff748640caab3
Laura3104 <71731102+Laura3104@users.noreply.github.com>
2020-12-11 15:17:54 -0500

    initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+        }
+        public DbSet<Value> Values {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>().HasData(
+                new Value {Id = 1, Name = "Laura 1"},
+                new Value {Id = 2, Name = "Laura 2"},
+                new Value {Id = 3, Name = "Laura 3"},
+                new Value {Id = 4, Name = "Laura 4"}
+            );
+        }
+    }
+}
